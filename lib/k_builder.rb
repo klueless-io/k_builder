10d49de0f1b814970f27282c9431e70401194cb0
klueless-io <david@ideasmen.com.au>
2021-03-11 09:46:05 +1100

    add configuration

--- lib/k_builder.rb
+++ lib/k_builder.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'k_builder/version'
+require 'k_builder/configuration'
+require 'k_builder/context'
 
 module KBuilder
   # raise KBuilder::Error, 'Sample message'
